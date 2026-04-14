6bd36086f6f88ea6c4066c262733c4041ac87ef6
quswodn1211@naver.com <72201917+alwayssaewoo@users.noreply.github.com>
2026-04-14 23:47:47 +0900

    0.1.2 update

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ ext_modules = [
 
 setup(
     name="hangultrans_bjw",
-    version="0.1.1",
+    version="0.1.2",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     ext_modules=ext_modules,
